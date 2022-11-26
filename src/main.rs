e5e857deec0e22924d1694ba065bb5915944799f
姚文强 <yaowenqiang111@163.com>
2022-11-26 23:43:22 +0800

    add lifetimes generic name demo

--- src/main.rs
+++ src/main.rs
@@ -685,6 +685,30 @@ fn main() {
     notify(&news);
     notify1(&news);
 
+    // let r;
+    // print!("r: {}", r);
+    // {
+    //     let x = 5;
+    //     r = &x;
+    // }
+
+    // print!("r: {}", r);
+
+    let string1 = String::from("abcd");
+    // let string2 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
 
 impl<T> Point<T> {
