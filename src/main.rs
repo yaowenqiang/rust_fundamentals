7f5af43b0d8f2a0bf321e6ba46d0b471dbfc863b
姚文强 <yaowenqiang111@163.com>
2022-12-02 23:42:36 +0800

    add static lifetime demo

--- src/main.rs
+++ src/main.rs
@@ -752,6 +752,26 @@ fn main() {
         }
     }
 
+    let s: &'static str = "I have a static lifetime.";
+
+
+
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
