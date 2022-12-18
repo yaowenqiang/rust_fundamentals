e06eb7776d9222eb26416bfac6cdc8a91e1d8122
姚文强 <yaowenqiang111@163.com>
2022-12-18 23:17:04 +0800

    add iterator demo

--- fp/src/main.rs
+++ fp/src/main.rs
@@ -116,7 +116,19 @@ fn main() {
     });
     println!("{:#?}, sorted in {num_sort_operations} operations.", rec_list);
 
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
 
-
-
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+}
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1,2,3];
+    let mut v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
 }
