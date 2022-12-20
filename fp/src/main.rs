9f3371e3c3c2a2f146c31c2b543344d0f40e48a3
姚文强 <yaowenqiang111@163.com>
2022-12-20 23:46:49 +0800

    add filter and iterator demo

--- fp/src/main.rs
+++ fp/src/main.rs
@@ -44,6 +44,16 @@ struct Rectangle {
 
 
 fn add_one_v1 (x: u32) -> u32 {x + 1}
+
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
 fn main() {
     let store = Inventory {
         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue]
@@ -126,6 +136,8 @@ fn main() {
     let v1 = vec![1,2,3];
     let v2 :Vec<_> = v1.iter().map(|x| x + 1).collect();
     assert_eq!(v2, vec![2,3,4]);
+
+
 }
 #[test]
 fn iterator_demonstration() {
@@ -143,4 +155,40 @@ fn iterator_sum() {
     let v1_iter = v1.iter();
     let total :i32 = v1_iter .sum();
     assert_eq!(total, 6);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe{
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe{
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe{
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(in_my_size, vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ])
+    }
+
 }
