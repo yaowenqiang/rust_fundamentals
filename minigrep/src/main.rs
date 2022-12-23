52de82b98b33cc3b01e3a9a5218196a2bfc78183
姚文强 <yaowenqiang111@163.com>
2022-12-23 20:37:06 +0800

    refactor the parse args part

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,24 @@
 use std::{env, fs};
+
+struct config {
+    query: String,
+     file_path: String,
+}
 fn main() {
     let args :Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for: {}", query);
-    println!("In file : {}", file_path);
+    // let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
+    println!("Searching for: {}", config.query);
+    println!("In file : {}", config.file_path);
     // dbg!(args);
 
     println!("In file {}", file_path);
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file.");
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config{query, file_path}
 }
