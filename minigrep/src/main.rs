e37ae50eb5fcb89223f69e55f9e4d1813cd13dac
姚文强 <yaowenqiang111@163.com>
2022-12-24 23:51:21 +0800

    add config new fn and error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,24 +1,34 @@
+#![feature(core_panic)]
+extern crate core;
+use core::panicking::panic;
 use std::{env, fs};
 
-struct config {
+
+struct Config {
     query: String,
      file_path: String,
 }
 fn main() {
     let args :Vec<String> = env::args().collect();
     // let (query, file_path) = parse_config(&args);
-    let config = parse_config(&args);
+    // let config = parse_config(&args);
+    let config = Config::new(&args);
     println!("Searching for: {}", config.query);
     println!("In file : {}", config.file_path);
     // dbg!(args);
 
-    println!("In file {}", file_path);
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file.");
+    // println!("In file {}", file_path);
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
     println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config{query, file_path}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config{query, file_path}
+    }
 }
