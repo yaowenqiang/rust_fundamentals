b02f5464700938c91a411d06e85494ae28d3bb25
姚文强 <yaowenqiang111@163.com>
2022-12-25 23:49:07 +0800

    add more error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,8 @@
 #![feature(core_panic)]
 extern crate core;
 use core::panicking::panic;
-use std::{env, fs};
+use std::{env, fs, process};
+use std::error::Error;
 
 
 struct Config {
@@ -12,14 +13,26 @@ fn main() {
     let args :Vec<String> = env::args().collect();
     // let (query, file_path) = parse_config(&args);
     // let config = parse_config(&args);
-    let config = Config::new(&args);
+    // let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     println!("Searching for: {}", config.query);
     println!("In file : {}", config.file_path);
     // dbg!(args);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
     // println!("In file {}", file_path);
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+    Ok(())
 }
 
 impl Config {
@@ -30,5 +43,13 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Config{query, file_path}
+    }
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config{query, file_path})
     }
 }
