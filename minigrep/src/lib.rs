a016e3f9c39ef76dad614ae0237bc5513eb8f34b
姚文强 <yaowenqiang111@163.com>
2022-12-26 23:32:00 +0800

    refactor, move some code to lib.rs

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,36 @@
+#![feature(core_panic)]
+extern crate core;
+use core::panicking::panic;
+use std::fs;
+use std::error::Error;
+
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    // println!("In file {}", file_path);
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text:\n{contents}");
+    Ok(())
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config{query, file_path}
+    }
+   pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config{query, file_path})
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,5 @@
-#![feature(core_panic)]
-extern crate core;
-use core::panicking::panic;
-use std::{env, fs, process};
-use std::error::Error;
-
-
-struct Config {
-    query: String,
-     file_path: String,
-}
+use std::{env, process};
+use minigrep::Config;
 fn main() {
     let args :Vec<String> = env::args().collect();
     // let (query, file_path) = parse_config(&args);
@@ -21,35 +12,9 @@ fn main() {
     println!("Searching for: {}", config.query);
     println!("In file : {}", config.file_path);
     // dbg!(args);
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    // println!("In file {}", file_path);
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
-    Ok(())
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Config{query, file_path}
-    }
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config{query, file_path})
-    }
 }
