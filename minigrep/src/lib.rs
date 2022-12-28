22598da4c7186e54da57e2a57a49a62d9c0968b0
姚文强 <yaowenqiang111@163.com>
2022-12-28 23:35:59 +0800

    pass the test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -13,6 +13,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     // println!("In file {}", file_path);
     let contents = fs::read_to_string(config.file_path)?;
     // println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
@@ -36,7 +39,13 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -51,6 +60,6 @@ mod tests {
 Rust:
 safe, fast, productive,
 Pick three.";
-        assert_eq!(vec!["safe, fast, productive"],search(query, contents));
+        assert_eq!(vec!["safe, fast, productive,"],search(query, contents));
     }
 }
