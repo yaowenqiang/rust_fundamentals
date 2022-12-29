4db8fa5f2eb8b4ebe99af48b6c861059f6c94c34
姚文强 <yaowenqiang111@163.com>
2022-12-29 23:45:56 +0800

    add ignore case search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,18 +1,24 @@
 #![feature(core_panic)]
 extern crate core;
 use core::panicking::panic;
-use std::fs;
+use std::{env, fs};
 use std::error::Error;
 
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     // println!("In file {}", file_path);
     let contents = fs::read_to_string(config.file_path)?;
     // println!("With text:\n{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
     for line in search(&config.query, &contents) {
         println!("{line}");
     }
@@ -20,21 +26,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Config{query, file_path}
-    }
    pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config{query, file_path})
+       let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config{
+            query,
+            file_path,
+            ignore_case
+        })
     }
 }
 
@@ -42,6 +45,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
