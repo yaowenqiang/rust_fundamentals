803f1aabc82002c21e70af98cc49f65b2fe9d200
姚文强 <yaowenqiang111@163.com>
2022-12-30 22:56:52 +0800

    write errot to std err

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     // let config = parse_config(&args);
     // let config = Config::new(&args);
     let config = Config::build(&args).unwrap_or_else(|err| {
-        // println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     // println!("Searching for: {}", config.query);
     // println!("In file : {}", config.file_path);
     // dbg!(args);
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 
