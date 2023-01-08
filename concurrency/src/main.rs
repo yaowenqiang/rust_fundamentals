b06bd846f7eb36cb8fe545fd4b07bfcee0403cc1
姚文强 <yaowenqiang111@163.com>
2023-01-08 23:39:36 +0800

    add thread join demo and thread move demo

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -2,15 +2,20 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
+    let v = vec![1,2,3];
 
-    for i in 1..10 {
-        println!("hi, number {} from the main thread", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    let handle = thread::spawn(move || {
+        // for i in 1..10 {
+        //     println!("hi number {} from the spawned thread!", i);
+        //     thread::sleep(Duration::from_millis(1));
+        // }
+        println!("Here's a vector: {:?}", v);
+    });
+    // for i in 1..10 {
+    //     println!("hi, number {} from the main thread", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    drop(v);
+    handle.join().unwrap();
 }
